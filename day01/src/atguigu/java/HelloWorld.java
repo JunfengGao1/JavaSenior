1c790e0f5c731f34fe22f394a4ac669153a8287e
JunfengGao <2578025765@qq.com>
2022-10-03 19:47:52 +0800

    push commit

--- day01/src/atguigu/java/HelloWorld.java
+++ day01/src/atguigu/java/HelloWorld.java
@@ -9,6 +9,8 @@ public class HelloWorld {
         System.out.println("git test4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
+
     }
 }
 
